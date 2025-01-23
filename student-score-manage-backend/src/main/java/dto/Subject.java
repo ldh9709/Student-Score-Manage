a2138b62e57182d727cacde3f2d2d3416e036add
ldh9709 <zszz5434@gmail.com>
2025-01-23 12:04:22 +0900

    feat : subjectDto 추가

---
+++ student-score-manage-backend/src/main/java/dto/Subject.java
@@ -0,0 +1,19 @@
+package dto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Subject {
+	
+	private Long subjectNo;
+	
+	private String subjectName;
+	
+}
