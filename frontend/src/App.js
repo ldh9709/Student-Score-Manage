6b32efc60b38296377346c752fc3ff4c1a1c79c1
ldh9709 <zszz5434@gmail.com>
2025-02-10 14:32:11 +0900

    feat : 어드민 페이지 PrivateRoute 설정

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,6 +7,7 @@ import StudentModifyPage from "./components/Admin/StudentModifyPage";
 import LoginPage from "./components/LoginPage";
 import RegisterPage from "./components/RegisterPage";
 import { AuthProvider } from "./util/AuthContext";
+import PrivateRoute from "./util/PrivateRoute";
 
 import "./App.css";
 
@@ -21,8 +22,10 @@ const App = () => {
           <Route path="/main" element={<MainPage />} />
           <Route path="/login" element={<LoginPage />} />
           <Route path="/register" element={<RegisterPage />} />
-          <Route path="/admin/*" element={<AdminPage />} />
-          <Route path="/admin/student/edit/:studentNo" element={<StudentModifyPage />} />
+          
+          {/* 인증된 사용자만 접근 가능 */}
+          <Route path="/admin/*" element={<PrivateRoute element={<AdminPage/>} />} />
+          <Route path="/admin/student/edit/:studentNo" element={<PrivateRoute element={<StudentModifyPage/>} />} />
         </Routes>
       </div>
     </Router>
